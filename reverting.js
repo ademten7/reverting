1bbe35f1eabb3276d31a0f78376fce734454457d
ademten82 <ademten7@gmail.com>
2021-04-24 11:00:48 +0200

    second commit

--- reverting.js
+++ reverting.js
@@ -1,1 +1,9 @@
 var n = [1,3,5];
+
+for (var i=0; i<n.length;i++){
+    if(n[i]%2==0){
+        console.log(n[i] +" is a even number")
+    }else{
+        console.log(n[i] + " is a odd number");
+    }
+}
